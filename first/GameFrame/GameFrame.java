2d3ef2a9d6ed6d8478c92a99d9021da49c9a79f2
assiqur191 <rahmanmdassiqur@gmail.com>
2021-09-13 21:06:47 +0600

    firstp

---
+++ first/GameFrame/GameFrame.java
@@ -0,0 +1,18 @@
+
+import java.awt.*;
+import javax.swing.*;
+
+public class GameFrame extends GFrame{
+
+
+    GameFrame(){
+
+   this.setDefaultcloseOperattion(GFream.EXIT_ON_CLOSE);
+   this.setsize(500,500);
+   this.setLocationRelativeTo(null);
+   this.setVisible(true);
+
+
+   }
+
+}
---
+++ first/GameProject.java
@@ -0,0 +1,19 @@
+import javax.swing.plaf.basic.BasicInternalFrameTitlePane.*;
+
+
+class GameProject{
+
+
+  
+public static void main(String[] args) {
+  
+   GemeFrame gf = new GrameFrame();
+   gf.printfunction();
+}
+
+
+
+
+
+
+}
